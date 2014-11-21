202ba5829b9e533331a1e05e81f5db6f0e9d41c3
nux <nux@nux.net>
2014-11-21 16:48:05 -0600

    extended class

--- search/MwsSearchClient.php
+++ search/MwsSearchClient.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__.'/src/MarketplaceWebServiceOrders/Client.php';
 
-class MwsSearchClient extends MarketplaceWebServiceProducts_Client 
+class MwsSearchClient extends MarketplaceWebServiceOrders_Client 
 {
     
 }
